cb8c3596924829777c24b015ff751ebf993cb0cd
Julius Michaelis <michaelis@fujitsu.com>
2024-10-28 22:36:03 +0900

    who knew there was a nicer way to create nixosConfigurations

--- flake.nix
+++ flake.nix
@@ -13,6 +13,7 @@
     nixpkgs,
   }: let
     inherit (pkgs.lib) mkForce getExe recursiveUpdate;
+    coreutil = pkgs.lib.getExe' pkgs.coreutils;
     pkgs = import nixpkgs {
       system = "x86_64-linux";
     };
@@ -126,169 +127,164 @@
       });
     in
       oa;
-    sys = nixpkgs.lib.nixosSystem {
-      inherit (pkgs) system;
-      modules = [
+    sys = pkgs.nixos {
+      boot.kernelPackages = pkgs.linuxPackagesFor linux;
+      # can't use boot.kernel.enable = false; we do want modules, but we don't have a kernel - any file will do
+      system.boot.loader.kernelFile = "bin/vmlinux";
+      boot.initrd.availableKernelModules = mkForce ["autofs4"]; # autofs is required by systemd, hostfs by this config
+      boot.initrd.kernelModules = mkForce ["hostfs"]; # bunch of modules we don't have or need (tpm, efi, …)
+      boot.loader.grub.enable = false; # needed for eval
+      boot.loader.initScript.enable = false; # unlike the documentation for this option says, not actually required.i
+      system.requiredKernelConfig = mkForce []; # systemd requires DMIID, but that requires DMI, and that doesn't exist on ARCH=um
+      boot.initrd.systemd.emergencyAccess = true;
+
+      fileSystems."/" = {
+        device = "tmp";
+        fsType = "tmpfs";
+      };
+      fileSystems.${builtins.storeDir} = {
+        device = "host";
+        fsType = "hostfs";
+        options = [builtins.storeDir];
+      };
+      fileSystems."/mnt/host" = {
+        device = "host";
+        fsType = "hostfs";
+      };
+      # some hackfixes for messing with the storeDir
+      boot.initrd.systemd.services.mkmounttargets = pkgs.lib.mkIf (builtins.storeDir != "/nix/store") {
+        # for some reason, initrd-parse-etc.service doesn't do it's thing.
+        unitConfig.DefaultDependencies = false;
+        wantedBy = ["initrd-fs.target"];
+        before = ["initrd-switch-root.service" "initrd-switch-root.service"];
+        script = ''
+          set -x
+          mount
+          if ! test -d /sysroot; then
+            mkdir -p /sysroot
+            mount -ttmpfs - /sysroot
+          fi
+          if ! test -d /sysroot/tmp/usmol1/nix/store; then
+            mkdir -p /sysroot/tmp/usmol1/nix/store /sysroot/mnt/host
+            mount -thostfs -o${builtins.storeDir} host /sysroot/${builtins.storeDir}
+          fi
+        '';
+      };
+      # Failed at step EXEC spawning …systemd-logind: No such file or directory
+      systemd.services.systemd-logind.enable = false; # It's just unnecessary?
+
+      networking.hostName = "lol"; # short for linux on linux. olo
+      boot.initrd.systemd.enable = true;
+      services.getty.autologinUser = "root";
+      services.journald.console = "tty1";
+
+      # startup is slow enough, disable some unused stuff
+      networking.firewall.enable = false;
+      networking.useDHCP = false;
+      services.logrotate.enable = false;
+      services.nscd.enable = false;
+      services.timesyncd.enable = false;
+      system.nssModules = mkForce [];
+      systemd.oomd.enable = false;
+      nix.enable = false; # doesn't build with alternate store paths
+      documentation.enable = false;
+
+      system.stateVersion = "24.11";
+
+      networking.interfaces.vec0.ipv4.addresses = [
         {
-          boot.kernelPackages = pkgs.linuxPackagesFor linux;
-          # can't use boot.kernel.enable = false; we do want modules, but we don't have a kernel - any file will do
-          system.boot.loader.kernelFile = "bin/vmlinux";
-          boot.initrd.availableKernelModules = mkForce ["autofs4"]; # autofs is required by systemd, hostfs by this config
-          boot.initrd.kernelModules = mkForce ["hostfs"]; # bunch of modules we don't have or need (tpm, efi, …)
-          boot.loader.grub.enable = false; # needed for eval
-          boot.loader.initScript.enable = false; # unlike the documentation for this option says, not actually required.i
-          system.requiredKernelConfig = mkForce []; # systemd requires DMIID, but that requires DMI, and that doesn't exist on ARCH=um
-          boot.initrd.systemd.emergencyAccess = true;
-
-          fileSystems."/" = {
-            device = "tmp";
-            fsType = "tmpfs";
-          };
-          fileSystems.${builtins.storeDir} = {
-            device = "host";
-            fsType = "hostfs";
-            options = [builtins.storeDir];
-          };
-          fileSystems."/mnt/host" = {
-            device = "host";
-            fsType = "hostfs";
-          };
-          # some hackfixes for messing with the storeDir
-          boot.initrd.systemd.services.mkmounttargets = nixpkgs.lib.mkIf (builtins.storeDir != "/nix/store") {
-            # for some reason, initrd-parse-etc.service doesn't do it's thing.
-            unitConfig.DefaultDependencies = false;
-            wantedBy = ["initrd-fs.target"];
-            before = ["initrd-switch-root.service" "initrd-switch-root.service"];
-            script = ''
-              set -x
-              mount
-              if ! test -d /sysroot; then
-                mkdir -p /sysroot
-                mount -ttmpfs - /sysroot
-              fi
-              if ! test -d /sysroot/tmp/usmol1/nix/store; then
-                mkdir -p /sysroot/tmp/usmol1/nix/store /sysroot/mnt/host
-                mount -thostfs -o${builtins.storeDir} host /sysroot/${builtins.storeDir}
-              fi
-            '';
-          };
-          # Failed at step EXEC spawning …systemd-logind: No such file or directory
-          systemd.services.systemd-logind.enable = false; # It's just unnecessary?
-
-          networking.hostName = "lol"; # short for linux on linux. olo
-          boot.initrd.systemd.enable = true;
-          services.getty.autologinUser = "root";
-          services.journald.console = "tty1";
-
-          # startup is slow enough, disable some unused stuff
-          networking.firewall.enable = false;
-          networking.useDHCP = false;
-          services.logrotate.enable = false;
-          services.nscd.enable = false;
-          services.timesyncd.enable = false;
-          system.nssModules = mkForce [];
-          systemd.oomd.enable = false;
-          nix.enable = false; # doesn't build with alternate store paths
-          documentation.enable = false;
-
-          system.stateVersion = "24.11";
-
-          networking.interfaces.vec0.ipv4.addresses = [
-            {
-              address = "10.0.2.100";
-              prefixLength = 24;
-            }
-          ];
-          networking.defaultGateway = "10.0.2.2";
-          networking.nameservers = ["10.0.2.3"];
-
-          virtualisation.docker.enable = true;
-          virtualisation.docker.extraOptions = "--iptables=False";
-          # todo
-          nixpkgs.overlays = [
-            (final: prev: {
-              iptables = final.iptables-legacy;
-            })
-          ];
-
-          systemd.services.compose-run.wantedBy = ["multi-user.target"];
-          systemd.services.compose-run.after = ["network.target" "docker.service"];
-          systemd.services.compose-run.serviceConfig.ExecStart = let
-            imageName = "stuff";
-            streamImage = pkgs.dockerTools.streamLayeredImage {
-              name = imageName;
-              tag = "latest";
-              fromImage = null;
-              contents = [pkgs.miniserve pkgs.wget];
-            };
-            port = 1337;
-
-            # standard-ish compose file
-            compose.services = {
-              serve = {
-                image = imageName;
-                command = ["miniserve" "-p${toString port}" "/mnt/data"];
-                expose = [port];
-                volumes = ["./data1:/mnt/data"];
-                healthcheck = {
-                  test = ["CMD" "wget" "-qO/dev/null" "http://localhost:${toString port}/"];
-                  interval = "5s";
-                };
-              };
-              get = {
-                image = imageName;
-                command = ["wget" "http://serve:${toString port}/canary" "-O/mnt/data/canary"];
-                volumes = ["./data2:/mnt/data"];
-                depends_on.serve.condition = "service_healthy";
-              };
-            };
-            # nonstandard part: we need extra_hosts since the internal dns relies on nat, which we don't have, and that needs static ips…
-            compose.networks.default = {
-              driver = "bridge";
-              ipam.config = [
-                {
-                  subnet = "10.5.0.0/16";
-                  "gateway" = "10.5.0.1";
-                }
-              ];
-            };
-            compose.services.serve.networks.default.ipv4_address = "10.5.0.5";
-            compose.services.get.extra_hosts.serve = "10.5.0.5";
-
-            composeFile = pkgs.writeText "docker-compose.yaml" (builtins.toJSON compose);
-            docker = getExe pkgs.docker;
-          in
-            pkgs.writeScript "run-compose" ''
-              #!${getExe pkgs.bash}
-              set -xeuo pipefail
-              ${streamImage} | ${docker} image load
-              cd "$(mktemp -d)"
-              mkdir data1 data2
-              echo $RANDOM >data1/canary
-              cp ${composeFile} docker-compose.yaml
-              ${docker} compose up --abort-on-container-exit --pull=never
-              cat data1/canary
-              cat data2/canary
-            '';
-          systemd.services.compose-run.serviceConfig.ExecStopPost = pkgs.writeScript "post-compose" ''
-            #!${getExe pkgs.bash}
-            set -eux
-            if test $SERVICE_RESULT == success; then
-              systemctl poweroff
-            else
-              # kernel panic to communicate the exit code. feels like sacrilege…
-              sync
-              echo c >/proc/sysrq-trigger
-            fi
-          '';
+          address = "10.0.2.100";
+          prefixLength = 24;
         }
       ];
+      networking.defaultGateway = "10.0.2.2";
+      networking.nameservers = ["10.0.2.3"];
+
+      virtualisation.docker.enable = true;
+      virtualisation.docker.extraOptions = "--iptables=False";
+      # todo
+      nixpkgs.overlays = [
+        (final: prev: {
+          iptables = final.iptables-legacy;
+        })
+      ];
+
+      systemd.services.compose-run.wantedBy = ["multi-user.target"];
+      systemd.services.compose-run.after = ["network.target" "docker.service"];
+      systemd.services.compose-run.serviceConfig.ExecStart = let
+        imageName = "stuff";
+        streamImage = pkgs.dockerTools.streamLayeredImage {
+          name = imageName;
+          tag = "latest";
+          fromImage = null;
+          contents = [pkgs.miniserve pkgs.wget];
+        };
+        port = 1337;
+
+        # standard-ish compose file
+        compose.services = {
+          serve = {
+            image = imageName;
+            command = ["miniserve" "-p${toString port}" "/mnt/data"];
+            expose = [port];
+            volumes = ["./data1:/mnt/data"];
+            healthcheck = {
+              test = ["CMD" "wget" "-qO/dev/null" "http://localhost:${toString port}/"];
+              interval = "5s";
+            };
+          };
+          get = {
+            image = imageName;
+            command = ["wget" "http://serve:${toString port}/canary" "-O/mnt/data/canary"];
+            volumes = ["./data2:/mnt/data"];
+            depends_on.serve.condition = "service_healthy";
+          };
+        };
+        # nonstandard part: we need extra_hosts since the internal dns relies on nat, which we don't have, and that needs static ips…
+        compose.networks.default = {
+          driver = "bridge";
+          ipam.config = [
+            {
+              subnet = "10.5.0.0/16";
+              "gateway" = "10.5.0.1";
+            }
+          ];
+        };
+        compose.services.serve.networks.default.ipv4_address = "10.5.0.5";
+        compose.services.get.extra_hosts.serve = "10.5.0.5";
+
+        composeFile = pkgs.writeText "docker-compose.yaml" (builtins.toJSON compose);
+        docker = getExe pkgs.docker;
+      in
+        pkgs.writeScript "run-compose" ''
+          #!${getExe pkgs.bash}
+          set -xeuo pipefail
+          ${streamImage} | ${docker} image load
+          cd "$(mktemp -d)"
+          mkdir data1 data2
+          echo $RANDOM >data1/canary
+          cp ${composeFile} docker-compose.yaml
+          ${docker} compose up --abort-on-container-exit --pull=never
+          cat data1/canary
+          cat data2/canary
+        '';
+      systemd.services.compose-run.serviceConfig.ExecStopPost = pkgs.writeScript "post-compose" ''
+        #!${getExe pkgs.bash}
+        set -eux
+        if test $SERVICE_RESULT == success; then
+          systemctl poweroff
+        else
+          # kernel panic to communicate the exit code. feels like sacrilege…
+          sync
+          echo c >/proc/sysrq-trigger
+        fi
+      '';
     };
     bin = pkgs.writeScriptBin "umlvm" ''
       #!${pkgs.runtimeShell} -eux
-      SOCKD="$(mktemp --directory)"
+      SOCKD="$(${coreutil "mktemp"} --directory)"
       SOCK="$SOCKD/slirp4netns-bess.sock"
-      trap 'rm -rf "$SOCKD"; trap - SIGTERM && kill -- -$$' SIGINT SIGTERM EXIT
+      trap '${coreutil "rm"} -rf "$SOCKD"; trap - SIGTERM && kill -- -$$' SIGINT SIGTERM EXIT
       ${getExe pkgs.slirp4netns} --target-type bess "$SOCK" &
       ${getExe linux} \
         mem=2G \
